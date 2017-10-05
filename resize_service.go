3442f5fca2c83decb35fcb0f0ef02766b585f756
Cheikh Seck <jvmstix@gmail.com>
2017-10-05 16:33:19 +0000

    Update resize_service.go

--- resize_service.go
+++ resize_service.go
@@ -1,31 +1,32 @@
 package resizer
 
-import (	
+import (
+	"fmt"
 	"github.com/disintegration/imaging"
 	"os"
-
+	"strings"
 )
+
 var CacheFolder = "./cache"
 
-func ResizeAndCache(imgpath string, cfolder string,width int , height int) (cacheref string,err error) {
-		//untested
-		cacheref := fmt.Sprintf("%s/%s_size_%v_%v.jpg", CacheFolder, strings.TrimPrefix(strings.Replace(imgpath,"/", ".", -1 ) , "."), width,height)
-  				if _, err = os.Stat(cacheref); !os.IsNotExist(err)  {				
-  						return 		
-  				} else {			
-					nimg, err := imaging.Open(imgpath)
-					if err != nil {
-						return
-					}
-					
-					newimage := imaging.Resize(nimg, width,height, imaging.Lanczos)
-					err = imaging.Save(newimage, cacheref)
-					if err != nil {
-						os.Remove(cacheref)
-						return 
-					}
-						
+func ResizeAndCache(imgpath string, cfolder string, width int, height int) (cacheref string, err error) {
+	//untested
+	cacheref = fmt.Sprintf("%s/%s_size_%v_%v.jpg", CacheFolder, strings.TrimPrefix(strings.Replace(imgpath, "/", ".", -1), "."), width, height)
+	if _, err = os.Stat(cacheref); os.IsNotExist(err) {
 
-					return
-			}
+		nimg, err := imaging.Open(imgpath)
+
+		if err != nil {
+			return cacheref, err
+		}
+
+		newimage := imaging.Resize(nimg, width, height, imaging.Lanczos)
+		err = imaging.Save(newimage, cacheref)
+		if err != nil {
+			os.Remove(cacheref)
+
+		}
+
+	}
+	return
 }
